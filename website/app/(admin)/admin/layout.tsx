7784a237b25a438cf97959c3df23d3d93aad9ae0
Ewerthon Kawan <ewerthonkawan@gmail.com>
2023-09-12 21:37:14 -0300

    Fix crash code

--- website/app/(admin)/admin/layout.tsx
+++ website/app/(admin)/admin/layout.tsx
@@ -1,5 +1,4 @@
 import { type Session, getServerSession } from 'next-auth';
-import { useTranslations } from 'next-intl';
 import { callbacks } from '@/app/api/auth/[...nextauth]/route';
 import { notFound } from 'next/navigation';
 import { Drawer } from '@/components/admin/header';
@@ -15,14 +14,8 @@ export default async function RootLayout({ children }: { children: React.ReactNo
 }
 
 const Content = ({ children, session }: { children: React.ReactNode; session: Session }) => {
-    const t = useTranslations('Components.Admin.Drawer');
-
     return (
-        <Drawer
-            drawerId="admin-drawer"
-            user={session.user}
-            texts={{ dashboard: t('dashboard'), products: t('products') }}
-        >
+        <Drawer drawerId="admin-drawer" user={session.user}>
             {children}
         </Drawer>
     );
